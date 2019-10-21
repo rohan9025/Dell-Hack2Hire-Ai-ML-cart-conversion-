3250bd281bbdc642ad5a4cce0ac93e4e0ee7b018
rohan9025 <nrgoun@gmail.com>
2019-10-21 23:12:50 +0530

    add other laptops-webpage

--- src/products/urls.py
+++ src/products/urls.py
@@ -5,5 +5,10 @@ from .import views
 urlpatterns = [
     path('', views.home,name='products-home'),
     path('about/', views.about,name='products-about'),
-    path('xps-webpage/',views.xps,name='xps-webpage')
+    path('xps-webpage/',views.xps,name='xps-webpage'),
+    path('latitude-webpage/',views.latitude,name='latitude-webpage'),
+    path('alienware-webpage/',views.alienware,name='alienware-webpage'),
+    path('inspiron-webpage/',views.inspiron,name='inspiron-webpage'),
+
+
 ]
